facebook/watchman

commit 4f89e886828ef0da6a0f8e9b7b7f8eea46fcaf7e

fix nasty wd -> name state issue
Summary:
In the case of moves, it was possible for our mapping of watch descriptor => path name to become out of date.

This diff addresses this issue by invalidating the source of a move before we pass it in for processing in the stat thread.

Test Plan:
replace the local watchman binary with the older version; run
`arc unit` to repro the failure.
Then make with this version and the test passes.

Reviewers: sid0

Reviewed By: sid0

CC: arice

Differential Revision: https://phabricator.fb.com/D790536

Task ID: 2336595, 2334495

arcanist/lib/WatchmanInstance.php
@@ -399,6 +399,9 @@ class WatchmanInstance {
     if (file_exists($this->vg_log.'.xml')) {
       copy($this->vg_log.'.xml', "/tmp/watchman-valgrind.xml");
     }
+    if (file_exists($this->vg_log)) {
+      copy($this->vg_log, "/tmp/watchman-valgrind.txt");
+    }
   }
 
   function __destruct() {

root.c
@@ -441,6 +441,25 @@ static void schedule_recrawl(w_root_t *root)
 }
 #endif
 
+#ifdef HAVE_INOTIFY_INIT
+static void invalidate_watch_descriptors(w_root_t *root,
+    struct watchman_dir *dir)
+{
+  w_ht_iter_t i;
+
+  if (w_ht_first(dir->dirs, &i)) do {
+    struct watchman_dir *child = (struct watchman_dir*)i.value;
+
+    invalidate_watch_descriptors(root, child);
+  } while (w_ht_next(dir->dirs, &i));
+
+  if (dir->wd != -1) {
+    w_ht_del(root->wd_to_dir, dir->wd);
+    dir->wd = -1;
+  }
+}
+#endif
+
 static void stop_watching_dir(w_root_t *root,
     struct watchman_dir *dir, bool do_close)
 {
@@ -1442,6 +1461,7 @@ static void inotify_thread(w_root_t *root)
     int n;
     struct timeval now;
     struct watchman_dir *dir;
+    struct watchman_file *file = NULL;
     w_string_t *name;
     struct pollfd pfd;
 
@@ -1507,10 +1527,48 @@ static void inotify_thread(w_root_t *root)
             }
           }
 
-          if (ine->len > 0) {
-            snprintf(buf, sizeof(buf), "%.*s/%.*s",
+          // We need to ensure that the watch descriptor associations from
+          // the old location are no longer valid so that when we crawl
+          // the destination location we'll update the entries
+          if (ine->len > 0 && (ine->mask & IN_MOVED_FROM)) {
+            name = w_string_new(ine->name);
+            file = w_root_resolve_file(root, dir, name, now);
+            if (!file) {
+              w_log(W_LOG_ERR,
+                  "looking for file %.*s but it is missing in %.*s\n",
+                  ine->len, ine->name, dir->path->len, dir->path->buf);
+              schedule_recrawl(root);
+              w_string_delref(name);
+              continue;
+            }
+
+            // The file no longer exists in its old location
+            file->exists = false;
+            w_root_mark_file_changed(root, file, now);
+            w_string_delref(name);
+
+            // Was there a dir here too?
+            snprintf(buf, sizeof(buf), "%.*s/%s",
                 dir->path->len, dir->path->buf,
-                ine->len, ine->name);
+                ine->name);
+            name = w_string_new(buf);
+
+            dir = w_root_resolve_dir(root, name, false);
+            if (dir) {
+              // Ensure that the old tree is not associated
+              invalidate_watch_descriptors(root, dir);
+              // and marked deleted
+              w_root_mark_deleted(root, dir, now, true);
+            }
+            w_string_delref(name);
+            continue;
+          }
+
+
+          if (ine->len > 0) {
+            snprintf(buf, sizeof(buf), "%.*s/%s",
+                dir->path->len, dir->path->buf,
+                ine->name);
             name = w_string_new(buf);
           } else {
             name = dir->path;
@@ -1537,8 +1595,8 @@ static void inotify_thread(w_root_t *root)
           // If we can't resolve the dir, and this isn't notification
           // that it has gone away, then we want to recrawl to fix
           // up our state.
-          w_log(W_LOG_ERR, "wanted dir %d, but not found %.*s\n",
-              ine->wd, ine->len, ine->name);
+          w_log(W_LOG_ERR, "wanted dir %d, but not found %s\n",
+              ine->wd, ine->name);
           schedule_recrawl(root);
         }
       }

tests/integration/moremoves.php
@@ -1,0 +1,58 @@
+<?php
+
+/* Copyright 2012-present Facebook, Inc.
+ * Licensed under the Apache License, Version 2.0 */
+
+class moremovesTestCase extends WatchmanTestCase {
+  function testMoreMoves() {
+    $dir = PhutilDirectoryFixture::newEmptyFixture();
+    $root = realpath($dir->getPath());
+    $watch = $this->watchmanCommand('watch', $root);
+    $base = $this->watchmanCommand('find', $root, '.');
+    // This is "c:PID:1" because nothing has changed in $root yet
+    $clock = $base['clock'];
+
+    system(
+      "cd $root; touch a; mkdir d1 d2 ; ".
+      "mv d1 d2 ; mv d2/d1 . ; mv a d1"
+    );
+
+    $this->assertFileListUsingSince($root, $clock,
+      array(
+        'd1',
+        'd1/a',
+        'd2'
+      )
+    );
+  }
+
+  function testEvenMoreMoves() {
+    $dir = PhutilDirectoryFixture::newEmptyFixture();
+    $root = realpath($dir->getPath());
+    $watch = $this->watchmanCommand('watch', $root);
+    $base = $this->watchmanCommand('find', $root, '.');
+    // This is "c:PID:1" because nothing has changed in $root yet
+    $clock = $base['clock'];
+
+    system(
+      "cd $root; ".
+      "mkdir d1 d2; ".
+      "touch d1/a; ".
+      "mkdir d3; ".
+      "mv d1 d2 d3; ".
+      "mv d3/* .; ".
+      "mv d1 d2 d3; ".
+      "mv d3/* .; ".
+      "mv d1/a d2; "
+    );
+
+    $this->assertFileListUsingSince($root, $clock,
+      array(
+        'd1',
+        'd2',
+        'd2/a',
+        'd3'
+      )
+    );
+  }
+}

commit 583fcda1f08179a8ad10f4cda0b5f2df6dbe4b48

Don't issue spurious recrawl for moves
Summary: 4f89e886828ef0d introduced this issue; when a dir is moved
we receive IN_MOVE_SELF but because we deleted our mapping we fall
into the something fishy code path.

Since there isn't any actionable information for us in IN_MOVE_SELF
(we have no way to know what path it is referring to),
we can ignore it and NOT consider it fishy.  We either have or will
receive a notification about its destination.

arcanist/lib/WatchmanInstance.php
@@ -108,6 +108,10 @@ class WatchmanInstance {
     return $data;
   }
 
+  function getLogData() {
+    return $this->logdata;
+  }
+
   function waitForLog($criteria, $timeout = 5) {
     foreach ($this->logdata as $line) {
       $matches = array();

root.c
@@ -1610,7 +1610,7 @@ static void inotify_thread(w_root_t *root)
 
           w_string_delref(name);
 
-        } else if ((ine->mask & IN_IGNORED) != IN_IGNORED) {
+        } else if ((ine->mask & (IN_MOVE_SELF|IN_IGNORED)) == 0) {
           // If we can't resolve the dir, and this isn't notification
           // that it has gone away, then we want to recrawl to fix
           // up our state.

tests/integration/fishy.php
@@ -1,0 +1,55 @@
+<?php
+
+/* Copyright 2013-present Facebook, Inc.
+ * Licensed under the Apache License, Version 2.0 */
+
+class fishyTestCase extends WatchmanTestCase {
+
+  function testFishy() {
+    $dir = PhutilDirectoryFixture::newEmptyFixture();
+    $root = realpath($dir->getPath());
+    mkdir("$root/foo");
+    touch("$root/foo/a");
+    $watch = $this->watchmanCommand('watch', $root);
+
+    $base = $this->watchmanCommand('find', $root, '.');
+    // This is "c:PID:1" because nothing has changed in $root yet
+    $clock = $base['clock'];
+
+    $this->setLogLevel('debug');
+    $this->watchmanCommand('log', 'debug', 'testFishy:START');
+
+    system(
+      "cd $root; ".
+      "mv foo bar; ".
+      "ln -s bar foo"
+    );
+
+    $this->assertFileListUsingSince($root, $clock,
+      array(
+        'bar',
+        'bar/a',
+        'foo'
+      )
+    );
+
+    $this->watchmanCommand('log', 'debug', 'testFishy:END');
+    $this->assertWaitForLog('/testFishy:END/');
+    $this->setLogLevel('off');
+    $on = false;
+    $log = array();
+    foreach (WatchmanInstance::get()->getLogData() as $item) {
+      if (preg_match('/testFishy:START/', $item)) {
+        $on = true;
+      } else if (preg_match('/testFishy:END/', $item)) {
+        break;
+      }
+      if ($on && preg_match('/fishy/', $item)) {
+        $log[] = $item;
+      }
+    }
+
+    $this->assertEqual(array(), $log, 'nothing fishy');
+  }
+}
+