d7c9e702e83dbd6a00190f3dd1a69eda9dc678c6
Wez Furlong <wez@fb.com>
2013-05-18 12:33:33 -0700

    add more coverage for the `name` expression

--- tests/integration/name.php
+++ tests/integration/name.php
@@ -55,6 +55,47 @@ class nameExprTestCase extends WatchmanTestCase {
       'fields' => array('name'),
     ));
     $this->assertEqual(array('subdir/bar.txt'), $res['files']);
+
+    $res = $this->watchmanCommand('query', $root, array(
+      'expression' => 'name',
+    ));
+    $this->assertEqual(
+      "failed to parse query: Expected array for 'name' term",
+      $res['error']
+    );
+
+    $res = $this->watchmanCommand('query', $root, array(
+      'expression' => array('name', 'one', 'two', 'three'),
+    ));
+    $this->assertEqual(
+      "failed to parse query: Invalid number of arguments for 'name' term",
+      $res['error']
+    );
+
+    $res = $this->watchmanCommand('query', $root, array(
+      'expression' => array('name', 2),
+    ));
+    $this->assertEqual(
+      "failed to parse query: Argument 2 to 'name' must be either ".
+      "a string or an array of string",
+      $res['error']
+    );
+
+    $res = $this->watchmanCommand('query', $root, array(
+      'expression' => array('name', 'one', 2),
+    ));
+    $this->assertEqual(
+      "failed to parse query: Argument 3 to 'name' must be a string",
+      $res['error']
+    );
+
+    $res = $this->watchmanCommand('query', $root, array(
+      'expression' => array('name', 'one', 'invalid'),
+    ));
+    $this->assertEqual(
+      "failed to parse query: Invalid scope 'invalid' for name expression",
+      $res['error']
+    );
   }
 }
 
