5aea055f642863fb7e093f26309a83f558a67438
Pascal Borreli <pascal@borreli.com>
2014-01-07 23:32:24 +0000

    Fixed typos
    Resolves https://github.com/facebook/watchman/pull/18
    
    applied using `hub am https://github.com/facebook/watchman/pull/18`

--- hash.c
+++ hash.c
@@ -106,7 +106,7 @@ uint32_t w_hash_bytes(const void *key, size_t length, uint32_t initval)
      * rest of the string.  Every machine with memory protection I've seen
      * does it on word boundaries, so is OK with this.  But VALGRIND will
      * still catch it and complain.  The masking trick does make the hash
-     * noticably faster for short strings (like English words).
+     * noticeably faster for short strings (like English words).
      */
     if (!running_on_valgrind()) {
 
