df46e00f63d40b6617991a43437822d3b39cdd3f
Thomas Klausner <wiz@NetBSD.org>
2014-08-10 10:45:40 +0200

    Use statvfs on NetBSD.
    One field name in struct statvfs is different than one Solaris; adapt.
    Fixes build on NetBSD, since statfs support was removed.

--- fstype.c
+++ fstype.c
@@ -53,11 +53,15 @@ w_string_t *w_fstype(const char *path)
   }
 
   return w_string_new(name);
-#elif defined(sun)
+#elif HAVE_SYS_STATVFS_H
   struct statvfs sfs;
 
   if (statvfs(path, &sfs) == 0) {
+#ifdef __NetBSD__
+    return w_string_new(sfs.f_fstypename);
+#else
     return w_string_new(sfs.f_basetype);
+#endif
   }
 #elif HAVE_STATFS
   struct statfs sfs;
