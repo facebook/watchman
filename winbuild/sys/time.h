d0265aec629baac1fb32ed60eb185432d6a2c4d0
Wez Furlong <wez@fb.com>
2014-12-30 09:47:58 -0800

    windows: add gettimeofday() and sleep functions
    These functions either resolve the current time or put us to sleep
    for some period of time.
    
    We use alertable wait states when we sleep so that our async IO
    can complete correctly.

---
+++ winbuild/sys/time.h
@@ -0,0 +1,37 @@
+/* Copyright 2014-present Facebook, Inc.
+ * Licensed under the Apache License, Version 2.0 */
+
+#ifndef SYS_TIME_H
+#define SYS_TIME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Defined by winsock, but we're not pulling that in, so we get
+// to define our own version of the struct.  We choose to define
+// tv_sec as a time_t for the sake of sanity.  This is not ABI
+// compatible with winsock, but we don't want or need that
+struct timeval {
+  time_t tv_sec;
+  time_t tv_usec;
+};
+struct timespec {
+  time_t tv_sec;
+  long long tv_nsec;
+};
+
+int gettimeofday(struct timeval *tv, void *ignored);
+void FILETIME_to_timespec(const FILETIME *ft, struct timespec *ts);
+void FILETIME_LARGE_INTEGER_to_timespec(LARGE_INTEGER ft, struct timespec *ts);
+void usleep(int64_t usec);
+void sleep(int sec);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* vim:ts=2:sw=2:et:
+ */
---
+++ winbuild/time.c
@@ -0,0 +1,62 @@
+/* Copyright 2014-present Facebook, Inc.
+ * Licensed under the Apache License, Version 2.0 */
+
+#include "watchman.h"
+
+// 100's of nanoseconds since the FILETIME epoch
+static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+// FILETIME is expressed in 100's of nanoseconds
+void FILETIME_LARGE_INTEGER_to_timespec(LARGE_INTEGER ft, struct timespec *ts) {
+  static const uint32_t factor = WATCHMAN_NSEC_IN_SEC / 100;
+
+  ft.QuadPart -= EPOCH;
+  ts->tv_sec  = ft.QuadPart / factor;
+  ft.QuadPart -= ts->tv_sec * factor;
+  ts->tv_nsec = ft.QuadPart * 100;
+}
+
+void FILETIME_to_timespec(const FILETIME *ft, struct timespec *ts) {
+  LARGE_INTEGER li;
+
+  li.HighPart = ft->dwHighDateTime;
+  li.LowPart = ft->dwLowDateTime;
+
+  FILETIME_LARGE_INTEGER_to_timespec(li, ts);
+}
+
+static void timespec_to_timeval(const struct timespec *ts, struct timeval *tv) {
+  tv->tv_sec = (long)ts->tv_sec;
+  tv->tv_usec = (long)(ts->tv_nsec / WATCHMAN_NSEC_IN_USEC);
+}
+
+int gettimeofday(struct timeval *tv, void *ignored) {
+  SYSTEMTIME  system_time;
+  FILETIME    file_time;
+  struct timespec ts;
+  unused_parameter(ignored);
+
+  GetSystemTime( &system_time );
+  SystemTimeToFileTime( &system_time, &file_time );
+  FILETIME_to_timespec(&file_time, &ts);
+  timespec_to_timeval(&ts, tv);
+  return 0;
+}
+
+void usleep(int64_t usec) {
+  HANDLE timer;
+  LARGE_INTEGER ft;
+
+  // Convert to 100 nanosecond interval, negative value indicates relative time
+  ft.QuadPart = -(10*usec);
+
+  timer = CreateWaitableTimer(NULL, TRUE, NULL);
+  SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+  // Use an alertable wait to play well with our stream implementation
+  WaitForSingleObjectEx(timer, INFINITE, true);
+  CloseHandle(timer);
+}
+
+void sleep(int sec) {
+  SleepEx(sec * 1000, true);
+}
