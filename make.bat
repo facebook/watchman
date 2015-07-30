7fa279b09bbd1530ca44b11a11984c2d9dcfe8fd
Wez Furlong <wez@fb.com>
2015-07-30 09:50:44 -0700

    windows: fixup make.bat
    Need to call the vs build environment script for things to work
    correctly

--- make.bat
+++ make.bat
@@ -1,7 +1,4 @@
-if "%VS120COMNTOOLS%" == "" (
-  @echo "Setting up build environment"
-  "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
-)
+call  "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 @rem Allow python build to succeed:
 @rem http://stackoverflow.com/questions/2817869/error-unable-to-find-vcvarsall-bat
 SET VS90COMNTOOLS=%VS120COMNTOOLS%
