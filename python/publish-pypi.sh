7fd548632236cad57bb4183d5f6626a6eb04576e
Kris Wilson <kwilson@twitter.com>
2015-08-06 17:38:54 -0700

    Pypi publishing support for watchman/python

---
+++ python/publish-pypi.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# ===============================
+# pywatchman pypi publishing tool
+# ===============================
+#
+# this publishes the watchman python module to the pypi project
+# index located at https://pypi.python.org/pypi/pywatchman. the
+# metadata used for this process comes directly from setup.py.
+#
+# this requires write access to the pywatchman project on pypi
+# as well as a ~/.pypirc of the following form:
+#
+#    [distutils]
+#    index-servers = pypi
+#
+#    [pypi]
+#    repository: https://pypi.python.org/pypi
+#    username: <pypi_username>
+#    password: <pypi_password>
+#
+
+python setup.py sdist upload -r pypi
