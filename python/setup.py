de1ca88bbef3052d716f6ef61c4d6ce83e2e7b18
Kris Wilson <kwilson@twitter.com>
2015-08-06 17:38:54 -0700

    Pypi publishing support for watchman/python

--- python/setup.py
+++ python/setup.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 # vim:ts=4:sw=4:et:
-from distutils.core import setup, Extension
+
+from setuptools import setup, Extension
 
 setup(
     name = 'pywatchman',
-    version = '0.1',
+    version = '0.1.1',
     description = 'Watchman client for python',
     author = 'Wez Furlong, Siddharth Agarwal',
+    author_email = 'wez@fb.com',
     maintainer = 'Wez Furlong',
     maintainer_email = 'wez@fb.com',
     url = 'https://github.com/facebook/watchman',
     long_description = 'Connect and query Watchman to discover file changes',
-    keywords = \
-        'watchman inotify fsevents kevent kqueue portfs filesystem watcher',
-    license = 'Apache 2.0',
+    keywords = (
+        'watchman inotify fsevents kevent kqueue portfs filesystem watcher'
+    ),
+    license = 'Apache License (2.0)',
     packages = ['pywatchman'],
     ext_modules = [
         Extension('pywatchman.bser', sources = ['pywatchman/bser.c'])
+    ],
+    platforms = 'Platform Independent',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: System :: Filesystems',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
     ]
 )
-
