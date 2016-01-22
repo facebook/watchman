aa4fb0d571477f5f96677a71897a9ae784ee8849
Wez Furlong <wez@fb.com>
2016-01-22 14:17:35 -0800

    Adjust windows build script
    This makes it succeed if the tools are in the path but the INCLUDE path
    isn't configured.

--- make.bat
+++ make.bat
@@ -1,6 +1,16 @@
+@rem if we don't have nmake in the path, we need to run setup
 where nmake.exe 2> NUL
-if %ERRORLEVEL% GTR 0 call  "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+if %ERRORLEVEL% GTR 0 set need_setup=1
+
+@rem if we don't have the include path set, we need to run setup
+if not defined INCLUDE set need_setup=1
+
+@rem run setup if we need to
+if %need_setup% == 1 call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+
 @rem Allow python build to succeed:
 @rem http://stackoverflow.com/questions/2817869/error-unable-to-find-vcvarsall-bat
 SET VS90COMNTOOLS=%VS120COMNTOOLS%
+
+@rem finally, run make
 nmake /nologo /s /f winbuild\Makefile %1
