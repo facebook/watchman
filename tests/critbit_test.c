b7665244f3660c89a0dfbf5d874e6f560aad1175
Wez Furlong <wez@fb.com>
2016-02-19 00:39:20 -0800

    add critbit tree data structure
    The intent is to use this to quickly answer whether we are interested
    in a specific file path.
    
    Imported from https://bitbucket.org/sid0/pycritbit and massaged to
    make the compiler happy, and added unit tests.

---
+++ tests/critbit_test.c
@@ -0,0 +1,165 @@
+/* Copyright 2016-present Facebook, Inc.
+ * Licensed under the Apache License, Version 2.0 */
+
+#include "thirdparty/tap.h"
+#include "thirdparty/critbit.h"
+#include <string.h>
+
+static void test_basic_simple(void) {
+  cb_tree_t tree = cb_tree_make();
+
+  ok(cb_tree_getitem(&tree, "foo") == NULL, "no foo in tree yet");
+
+  ok(!cb_tree_contains(&tree, "foo"), "no foo");
+  ok(cb_tree_setitem(&tree, "foo", "1", NULL) == 0, "stored foo -> 1");
+  ok(cb_tree_contains(&tree, "foo"), "contains foo");
+
+  ok(cb_tree_setdefault(&tree, "foo", "wat", NULL) == -1,
+     "setdefault can't replace existing value");
+
+  ok(!cb_tree_contains(&tree, "bar"), "no bar");
+  ok(cb_tree_setdefault(&tree, "bar", "2", NULL) == 0, "stored bar -> 2");
+  ok(cb_tree_contains(&tree, "bar"), "contains bar");
+
+  ok(tree.count == 2, "2 elements, have %d", tree.count);
+
+  ok(!strcmp(cb_tree_getitem(&tree, "foo"), "1"), "should have gotten 1");
+  ok(!strcmp(cb_tree_getitem(&tree, "bar"), "2"), "should have gotten 2");
+
+  ok(cb_tree_setitem(&tree, "foo", "3", NULL) == 1, "stored foo -> 3");
+  ok(tree.count == 2, "still have 2 elements, have %d", tree.count);
+  ok(!strcmp(cb_tree_getitem(&tree, "foo"), "3"), "should have gotten 3");
+
+  ok(cb_tree_delete(&tree, "bar", NULL, NULL) == 0, "removed bar");
+  ok(tree.count == 1, "now have 1 element, have %d", tree.count);
+  ok(!strcmp(cb_tree_getitem(&tree, "foo"), "3"), "should have gotten 3");
+  ok(cb_tree_getitem(&tree, "bar") == NULL, "bar should be gone");
+
+  cb_tree_clear(&tree);
+  ok(tree.count == 0, "no more entries");
+}
+
+static void test_basic_popitem(void) {
+  cb_tree_t tree = cb_tree_make();
+  const char *key, *value;
+
+  ok(cb_tree_setitem(&tree, "foo1", "1", NULL) == 0, "stored foo1");
+  ok(cb_tree_setitem(&tree, "foo2", "2", NULL) == 0, "stored foo2");
+  ok(cb_tree_setitem(&tree, "foo12", "3", NULL) == 0, "stored foo12");
+
+  // Note the order of the pops
+  ok(cb_tree_popitem(&tree, (const void **)&key, (const void **)&value) == 0,
+     "popped");
+  ok(!strcmp(key, "foo1"), "foo1");
+  ok(!strcmp(value, "1"), "value: 1");
+
+  ok(cb_tree_popitem(&tree, (const void **)&key, (const void **)&value) == 0,
+     "popped");
+  ok(!strcmp(key, "foo12"), "foo12");
+  ok(!strcmp(value, "3"), "value: 3");
+
+  ok(cb_tree_popitem(&tree, (const void **)&key, (const void **)&value) == 0,
+     "popped");
+  ok(!strcmp(key, "foo2"), "foo2");
+  ok(!strcmp(value, "2"), "value: 2");
+
+  ok(tree.count == 0, "no more entries");
+  cb_tree_clear(&tree);
+}
+
+static void test_basic_has_prefix(void) {
+  cb_tree_t tree = cb_tree_make();
+
+  ok(cb_tree_has_prefix_str(&tree, "", 0) == 0,
+     "doesn't match empty string prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo", 3) == 0, "doesn't match foo prefix");
+
+  ok(cb_tree_setitem(&tree, "foo1", "1", NULL) == 0, "inserted foo1");
+
+  ok(cb_tree_has_prefix_str(&tree, "", 0) == 1, "matches empty prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo", 3) == 1, "matches foo prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo1", 4) == 1, "matches foo1 prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo12", 5) == 0, "no foo12 prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo2", 4) == 0, "no foo2 prefix");
+
+  ok(cb_tree_setitem(&tree, "foo12", "2", NULL) == 0, "inserted foo12");
+
+  ok(cb_tree_has_prefix_str(&tree, "", 0) == 1, "matches empty prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo", 3) == 1, "matches foo prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo1", 4) == 1, "matches foo1 prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo12", 5) == 1, "matches foo12 prefix");
+  ok(cb_tree_has_prefix_str(&tree, "foo2", 4) == 0, "no foo2 prefix");
+
+  cb_tree_clear(&tree);
+}
+
+static void test_basic_iter(void) {
+  cb_tree_t tree = cb_tree_make();
+  cb_iter_t iter;
+  const char *key, *value;
+
+  ok(cb_tree_setitem(&tree, "foo1", "1", NULL) == 0, "stored foo1");
+  ok(cb_tree_setitem(&tree, "foo2", "2", NULL) == 0, "stored foo2");
+  ok(cb_tree_setitem(&tree, "foo12", "3", NULL) == 0, "stored foo12");
+
+  iter = cb_tree_iter(&tree);
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 1, "iter 1");
+  ok(!strcmp(key, "foo1"), "foo1");
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 1, "iter 2");
+  ok(!strcmp(key, "foo12"), "foo12");
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 1, "iter 3");
+  ok(!strcmp(key, "foo2"), "foo2");
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 0, "no more items");
+
+  // Run through against to verify the values
+  iter = cb_tree_iter(&tree);
+
+  ok(cb_tree_iter_next(&iter, NULL, (void **)&value) == 1, "iter 1");
+  ok(!strcmp(value, "1"), "foo1");
+
+  ok(cb_tree_iter_next(&iter, NULL, (void **)&value) == 1, "iter 2");
+  ok(!strcmp(value, "3"), "foo12");
+
+  ok(cb_tree_iter_next(&iter, NULL, (void **)&value) == 1, "iter 3");
+  ok(!strcmp(value, "2"), "foo2");
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 0, "no more items");
+
+  // Now test that prefix iteration works
+  ok(cb_tree_setitem(&tree, "bar", "b", NULL) == 0, "stored bar");
+
+  iter = cb_tree_iter_prefix_str(&tree, "foo", 3);
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 1, "iter 1");
+  ok(!strcmp(key, "foo1"), "foo1");
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 1, "iter 2");
+  ok(!strcmp(key, "foo12"), "foo12");
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 1, "iter 3");
+  ok(!strcmp(key, "foo2"), "foo2");
+
+  ok(cb_tree_iter_next(&iter, (void **)&key, NULL) == 0, "no more items");
+
+  cb_tree_clear(&tree);
+}
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+
+  plan_tests(71);
+  test_basic_simple();
+  test_basic_popitem();
+  test_basic_has_prefix();
+  test_basic_iter();
+
+  return exit_status();
+}
+
+/* vim:ts=2:sw=2:et:
+ */
---
+++ thirdparty/critbit.c
@@ -0,0 +1,539 @@
+/*
+ * critbit89 - A crit-bit tree implementation for strings in C89
+ * Written by Jonas Gehring <jonas@jgehring.net>
+ * Portions copyright 2013 Facebook.
+ */
+
+/*
+ * The code makes the assumption that malloc returns pointers aligned at at
+ * least a two-byte boundary. Since the C standard requires that malloc return
+ * pointers that can store any type, there are no commonly-used toolchains for
+ * which this assumption is false.
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "critbit.h"
+
+typedef struct cb_node_t {
+  uint32_t child[2];
+  uint32_t byte;
+  uint8_t otherbits;
+} cb_node_t;
+
+typedef struct cb_node_kv_pair_t {
+  cb_node_t i;
+  cb_kv_pair_t e;
+} cb_node_kv_pair_t;
+
+#define INVALID_NODE (uint32_t) - 1
+
+static void *realloc_and_zero(void *ptr, size_t oldsize, size_t newsize) {
+  ptr = realloc(ptr, newsize);
+  memset(ptr + oldsize, 0, newsize - oldsize);
+  return ptr;
+}
+
+static int32_t new_node(cb_tree_t *tree) {
+  if (tree->_next_node >= tree->_node_size) {
+    int32_t newsize = tree->_node_size * 2;
+    tree->_nodes = realloc_and_zero(tree->_nodes, tree->_node_size *
+                                                      sizeof(cb_node_kv_pair_t),
+                                    newsize * sizeof(cb_node_kv_pair_t));
+    tree->_node_size = newsize;
+  }
+  return tree->_next_node++;
+}
+
+static void free_inode(cb_tree_t *tree, int32_t node) {
+  /* This does nothing for now. */
+  (void)tree; /* prevent compiler warnings */
+  (void)node;
+}
+
+static void free_enode(cb_tree_t *tree, int32_t node) {
+  tree->_nodes[node].e.k = NULL;
+  tree->_nodes[node].e.v = NULL;
+}
+
+static const char *key_getter_std(const void *k) { return (const char *)k; }
+
+static ssize_t key_size_std(const void *k) { return strlen((const char *)k); }
+
+static void kv_hook_func_std(void *k, void *v) {
+  /* Since we only store references, this should do nothing. */
+  (void)k; /* prevent compiler warnings */
+  (void)v;
+  return;
+}
+
+static void _init(cb_tree_t *tree) {
+  tree->root = INVALID_NODE;
+  tree->count = 0;
+  tree->_last_sorted = NULL;
+  tree->_nodes = calloc(16, sizeof(cb_node_kv_pair_t));
+  tree->_node_size = 16;
+  tree->_next_node = 0;
+}
+
+cb_tree_t cb_tree_make(void) {
+  cb_tree_t tree;
+  _init(&tree);
+  tree.key_getter = &key_getter_std;
+  tree.key_size = &key_size_std;
+  tree.on_clear = &kv_hook_func_std;
+  tree.on_copy = &kv_hook_func_std;
+  return tree;
+}
+
+void *cb_tree_getitem(cb_tree_t *tree, const void *k) {
+  const cb_node_kv_pair_t *nodes = tree->_nodes;
+  const char *str = tree->key_getter(k);
+  const uint8_t *ubytes = (void *)str;
+  const ssize_t ulen = tree->key_size(k);
+  uint32_t p = tree->root;
+  uint32_t pairindex;
+
+  if (p == INVALID_NODE) {
+    return NULL;
+  }
+
+  while (1 & p) {
+    uint32_t q = p >> 1;
+    uint8_t c = 0;
+    int direction;
+
+    if (nodes[q].i.byte < ulen) {
+      c = ubytes[nodes[q].i.byte];
+    }
+    direction = (1 + (nodes[q].i.otherbits | c)) >> 8;
+
+    p = nodes[q].i.child[direction];
+  }
+
+  pairindex = p >> 1;
+  if (memcmp(str, tree->key_getter(nodes[pairindex].e.k),
+             tree->key_size(nodes[pairindex].e.k)) == 0) {
+    return nodes[pairindex].e.v;
+  }
+  return NULL;
+}
+
+int cb_tree_contains(cb_tree_t *tree, const void *k) {
+  return (cb_tree_getitem(tree, k) != NULL);
+}
+
+static int _setitem(cb_tree_t *tree, void *k, void *v, int replace,
+                    const void **oldv) {
+  cb_node_kv_pair_t *nodes = tree->_nodes;
+  const char *str = tree->key_getter(k);
+  const uint8_t *const ubytes = (void *)str;
+  const ssize_t ulen = tree->key_size(k);
+  uint32_t p = tree->root;
+  uint32_t pairindex;
+  const uint8_t *pairkeystr;
+  uint8_t c;
+  uint32_t newbyte;
+  uint32_t newotherbits;
+  int direction, newdirection;
+  uint32_t newnodeindex;
+  uint32_t *wherep;
+
+  if (p == INVALID_NODE) {
+    pairindex = new_node(tree);
+    nodes[pairindex].e.k = k;
+    nodes[pairindex].e.v = v;
+    tree->root = pairindex << 1;
+    tree->count = 1;
+    tree->_last_sorted = ubytes;
+    return 0;
+  }
+
+longest_common_prefix_search:
+  if (tree->_last_sorted) {
+    /*
+    This is a really neat optimization for when elements are
+    inserted in sorted order. When they are, then one of the
+    elements that shares the longest common prefix with k is the
+    last element inserted. Compare against that, and if our
+    assumption fails fall back to a tree search.
+    */
+    pairkeystr = tree->_last_sorted;
+    pairindex = tree->count - 1;
+  } else {
+    while (1 & p) {
+      uint32_t q = p >> 1;
+      c = 0;
+      if (nodes[q].i.byte < ulen) {
+        c = ubytes[nodes[q].i.byte];
+      }
+      direction = (1 + (nodes[q].i.otherbits | c)) >> 8;
+
+      p = nodes[q].i.child[direction];
+    }
+
+    pairindex = p >> 1;
+    /* This needs to be unsigned because it's compared against ubytes */
+    pairkeystr = (uint8_t *)tree->key_getter(nodes[pairindex].e.k);
+  }
+
+  for (newbyte = 0; newbyte < ulen; ++newbyte) {
+    if (pairkeystr[newbyte] != ubytes[newbyte]) {
+      newotherbits = pairkeystr[newbyte] ^ ubytes[newbyte];
+      goto different_byte_found;
+    }
+  }
+  if (pairkeystr[newbyte] != 0) {
+    newotherbits = pairkeystr[newbyte];
+    goto different_byte_found;
+  }
+
+  /* Key already in the tree -- replace */
+  if (oldv) {
+    *oldv = nodes[pairindex].e.v;
+  }
+  if (replace) {
+    nodes[pairindex].e.v = v;
+    return 1;
+  }
+  return -1;
+
+different_byte_found:
+  newotherbits |= newotherbits >> 1;
+  newotherbits |= newotherbits >> 2;
+  newotherbits |= newotherbits >> 4;
+  newotherbits = (newotherbits & ~(newotherbits >> 1)) ^ 255;
+  c = pairkeystr[newbyte];
+  newdirection = (1 + (newotherbits | c)) >> 8;
+
+  /* newdirection is 1 for a lexicographically smaller element */
+  if (newdirection && tree->_last_sorted) {
+    /* whoops, the optimization failed */
+    tree->_last_sorted = NULL;
+    goto longest_common_prefix_search;
+  }
+
+  newnodeindex = new_node(tree);
+  if (newnodeindex == INVALID_NODE) {
+    return -1;
+  }
+  /* possible realloc */
+  nodes = tree->_nodes;
+
+  nodes[newnodeindex].i.byte = newbyte;
+  nodes[newnodeindex].i.otherbits = newotherbits;
+  nodes[newnodeindex].i.child[1 - newdirection] = newnodeindex << 1;
+  nodes[newnodeindex].e.k = k;
+  nodes[newnodeindex].e.v = v;
+  if (oldv) {
+    *oldv = NULL;
+  }
+
+  /* Insert into tree */
+  wherep = &tree->root;
+  for (;;) {
+    uint32_t q;
+    p = *wherep;
+    if (!(1 & p)) {
+      break;
+    }
+
+    q = p >> 1;
+    if (nodes[q].i.byte > newbyte) {
+      break;
+    }
+    if (nodes[q].i.byte == newbyte && nodes[q].i.otherbits > newotherbits) {
+      break;
+    }
+
+    c = 0;
+    if (nodes[q].i.byte < ulen) {
+      c = ubytes[nodes[q].i.byte];
+    }
+    direction = (1 + (nodes[q].i.otherbits | c)) >> 8;
+    wherep = &nodes[q].i.child[direction];
+  }
+
+  nodes[newnodeindex].i.child[newdirection] = *wherep;
+  *wherep = (newnodeindex << 1) + 1;
+  tree->count++;
+  if (tree->_last_sorted)
+    tree->_last_sorted = ubytes;
+  return 0;
+}
+
+int cb_tree_setitem(cb_tree_t *tree, void *k, void *v, const void **oldv) {
+  return _setitem(tree, k, v, 1, oldv);
+}
+
+int cb_tree_setdefault(cb_tree_t *tree, void *k, void *v, const void **oldv) {
+  return _setitem(tree, k, v, 0, oldv);
+}
+
+/*! Deletes str from the tree, returns 0 on success */
+int cb_tree_delete(cb_tree_t *tree, const void *k, const void **oldk,
+                   const void **oldv) {
+  cb_node_kv_pair_t *nodes = tree->_nodes;
+  const char *str = tree->key_getter(k);
+  const uint8_t *ubytes = (void *)str;
+  const ssize_t ulen = tree->key_size(k);
+  uint32_t p = tree->root;
+  uint32_t *wherep = 0, *whereq = 0;
+  uint32_t pairindex;
+  uint32_t q /*= -1*/;
+  int direction = 0;
+
+  if (p == INVALID_NODE) {
+    return 1;
+  }
+  wherep = &tree->root;
+
+  while (1 & p) {
+    uint8_t c = 0;
+    whereq = wherep;
+    q = p >> 1;
+
+    if (nodes[q].i.byte < ulen) {
+      c = ubytes[nodes[q].i.byte];
+    }
+    direction = (1 + (nodes[q].i.otherbits | c)) >> 8;
+    wherep = &nodes[q].i.child[direction];
+    p = *wherep;
+  }
+
+  pairindex = p >> 1;
+  if (memcmp(str, tree->key_getter(nodes[pairindex].e.k),
+             tree->key_size(nodes[pairindex].e.k)) != 0) {
+    return 1;
+  }
+
+  if (oldk) {
+    *oldk = nodes[pairindex].e.k;
+  }
+  if (oldv) {
+    *oldv = nodes[pairindex].e.v;
+  }
+
+  free_enode(tree, pairindex);
+  tree->count--;
+  /* don't bother with the last_sorted optimization */
+  tree->_last_sorted = NULL;
+
+  if (!whereq) {
+    tree->root = -1;
+    return 0;
+  }
+
+  *whereq = nodes[q].i.child[1 - direction];
+  free_inode(tree, q);
+  return 0;
+}
+
+int cb_tree_popitem(cb_tree_t *tree, const void **k, const void **v) {
+  cb_node_kv_pair_t *nodes = tree->_nodes;
+  uint32_t p = tree->root;
+  uint32_t *wherep = 0, *whereq = 0;
+  uint32_t pairindex;
+
+  uint32_t q /* = -1*/;
+  if (p == INVALID_NODE)
+    return 1;
+
+  wherep = &tree->root;
+
+  while (1 & p) {
+    whereq = wherep;
+    q = p >> 1;
+    wherep = &nodes[q].i.child[0];
+    p = *wherep;
+  }
+
+  pairindex = p >> 1;
+  if (k) {
+    *k = nodes[pairindex].e.k;
+  }
+  if (v) {
+    *v = nodes[pairindex].e.v;
+  }
+  free_enode(tree, pairindex);
+  tree->count--;
+  /* the last_sorted optimization is still fine because the
+  lexicographically first element is removed... */
+
+  if (!whereq) {
+    tree->root = -1;
+    /* ...unless there's just one element, of course */
+    tree->_last_sorted = NULL;
+    return 0;
+  }
+
+  *whereq = nodes[q].i.child[1];
+  free_inode(tree, q);
+  return 0;
+}
+
+void cb_tree_clear(cb_tree_t *tree) {
+  cb_node_kv_pair_t *nodes = tree->_nodes;
+  int32_t i;
+  int32_t size = tree->_node_size;
+  cb_kv_hook_func onclear = tree->on_clear;
+
+  for (i = 0; i < size; i++) {
+    if (nodes[i].e.k)
+      onclear(nodes[i].e.k, nodes[i].e.v);
+  }
+  free(nodes);
+  _init(tree);
+}
+
+cb_tree_t cb_tree_copy(cb_tree_t *tree) {
+  cb_tree_t newtree;
+  int32_t i;
+  size_t size = tree->_node_size * sizeof(cb_node_kv_pair_t);
+  newtree.root = tree->root;
+  newtree.count = tree->count;
+  newtree.key_getter = tree->key_getter;
+  newtree.key_size = tree->key_size;
+  newtree.on_clear = tree->on_clear;
+  newtree.on_copy = tree->on_copy;
+
+  newtree._nodes = malloc(size);
+  memcpy(newtree._nodes, tree->_nodes, size);
+  newtree._node_size = tree->_node_size;
+  newtree._next_node = tree->_next_node;
+  for (i = 0; i < newtree._node_size; i++) {
+    if (newtree._nodes[i].e.k)
+      newtree.on_copy(newtree._nodes[i].e.k, newtree._nodes[i].e.v);
+  }
+  return newtree;
+}
+
+/* Returns the part of the tree containing this prefix, or NULL if not found */
+static uint32_t _find_prefix(cb_tree_t *tree, const char *prefix_str,
+                             ssize_t ulen) {
+  const cb_node_kv_pair_t *nodes = tree->_nodes;
+  uint8_t *ubytes;
+  uint32_t p = tree->root;
+  uint32_t top = p;
+  const char *str;
+
+  if (prefix_str == NULL)
+    return p;
+  if (top == INVALID_NODE)
+    return -1;
+
+  ubytes = (void *)prefix_str;
+
+  while (1 & p) {
+    uint32_t q = p >> 1;
+    uint8_t c = 0;
+    int direction;
+
+    if (nodes[q].i.byte < ulen) {
+      c = ubytes[nodes[q].i.byte];
+    }
+    direction = (1 + (nodes[q].i.otherbits | c)) >> 8;
+
+    p = nodes[q].i.child[direction];
+    if (nodes[q].i.byte < ulen)
+      top = p;
+  }
+
+  /* Check that what we found is actually the prefix */
+  str = tree->key_getter(nodes[p >> 1].e.k);
+  if (tree->key_size(nodes[p >> 1].e.k) >= ulen &&
+      memcmp(str, prefix_str, ulen) == 0)
+    return top;
+  else
+    return -1;
+}
+
+int cb_tree_has_prefix_key(cb_tree_t *tree, const void *prefix_key) {
+  const char *str = NULL;
+  ssize_t size = -1;
+  if (prefix_key != NULL) {
+    str = tree->key_getter(prefix_key);
+    size = tree->key_size(prefix_key);
+  }
+  return cb_tree_has_prefix_str(tree, str, size);
+}
+
+int cb_tree_has_prefix_str(cb_tree_t *tree, const char *prefix_str,
+                           ssize_t prefix_size) {
+  return (_find_prefix(tree, prefix_str, prefix_size) != INVALID_NODE);
+}
+
+cb_iter_t cb_tree_iter(cb_tree_t *tree) {
+  return cb_tree_iter_prefix_str(tree, NULL, -1);
+}
+
+cb_iter_t cb_tree_iter_prefix_key(cb_tree_t *tree, const void *prefix_key) {
+  const char *str = NULL;
+  ssize_t size = -1;
+  if (prefix_key != NULL) {
+    str = tree->key_getter(prefix_key);
+    size = tree->key_size(prefix_key);
+  }
+  return cb_tree_iter_prefix_str(tree, str, size);
+}
+
+cb_iter_t cb_tree_iter_prefix_str(cb_tree_t *tree, const char *prefix_str,
+                                  ssize_t prefix_size) {
+  cb_iter_t rv = {tree, NULL};
+
+  uint32_t top = _find_prefix(tree, prefix_str, prefix_size);
+  if (top != INVALID_NODE) {
+    rv.tree = tree;
+    rv.head = malloc(sizeof(cb_iter_stack));
+    rv.head->node = top;
+    rv.head->next = NULL;
+  }
+  return rv;
+}
+
+/*! Returns 1 if k and v are returned, 0 if exhausted, -1 on error */
+int cb_tree_iter_next(cb_iter_t *iter, void **k, void **v) {
+  const cb_node_kv_pair_t *nodes = iter->tree->_nodes;
+  /* At any time we will remove exactly one node from the stack */
+  uint32_t currnode;
+  cb_iter_stack *newhead;
+  uint32_t pairindex;
+
+  if (iter->head == NULL) {
+    if (k != NULL) {
+      *k = NULL;
+    }
+    if (v != NULL) {
+      *v = NULL;
+    }
+    return 0;
+  }
+
+  currnode = iter->head->node;
+  newhead = iter->head->next;
+  free(iter->head);
+  while (1 & currnode) {
+    uint32_t q = currnode >> 1;
+    cb_iter_stack *newnode = malloc(sizeof(cb_iter_stack));
+    if (newnode == NULL) {
+      iter->head = NULL;
+      return -1;
+    }
+    newnode->node = nodes[q].i.child[1];
+    newnode->next = newhead;
+    newhead = newnode;
+    currnode = nodes[q].i.child[0];
+  }
+  iter->head = newhead;
+  pairindex = currnode >> 1;
+  if (k != NULL) {
+    *k = nodes[pairindex].e.k;
+  }
+  if (v != NULL) {
+    *v = nodes[pairindex].e.v;
+  }
+  return 1;
+}
