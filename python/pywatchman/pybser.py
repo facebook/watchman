2664fabb6bf6c896c3bbe9c685282a4cae8d80a1
Siddharth Agarwal <sid0@fb.com>
2016-05-17 14:55:00 -0700

    pywatchman: don't enable unicode literals on Python 2
    We'd like to use native strings in some places (bytes in Python 2, Unicode in
    Python 3). unicode_literals prevents us from doing that.
    
    See http://python-future.org/unicode_literals.html for more discussion about this.

--- python/pywatchman/pybser.py
+++ python/pywatchman/pybser.py
@@ -29,7 +29,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import unicode_literals
+# no unicode literals
 
 import binascii
 import collections
--- python/tests/tests.py
+++ python/tests/tests.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import unicode_literals
+# no unicode literals
 
 import binascii
 import inspect
