82a73e39f7ef2634abe47188980a1542f6cc021e
Siddharth Agarwal <sid0@fb.com>
2016-05-16 22:59:06 -0700

    pywatchman: add Python 3 compatibility imports to capabilities.py
    We'll still need to handle bytestrings appropriately, but we can defer that to
    later patches.

--- python/pywatchman/capabilities.py
+++ python/pywatchman/capabilities.py
@@ -26,6 +26,11 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# no unicode literals
+
 import re
 
 def parse_version(vstr):
