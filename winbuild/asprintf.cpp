a825bb2834ba457db29942e69c036892f582bdae
Wez Furlong <wez@fb.com>
2016-06-26 11:50:09 -0700

    winbuild/asprintf.c -> winbuild/asprintf.cpp

--- winbuild/asprintf.cpp
+++ winbuild/asprintf.cpp
@@ -9,7 +9,7 @@ int vasprintf(char **out, WATCHMAN_FMT_STRING(const char *fmt), va_list ap) {
   char stack[512];
   char *buf = stack;
   char *newbuf;
-  int alloc = sizeof(stack);
+  unsigned int alloc = sizeof(stack);
   int len;
 
   while (1) {
@@ -28,7 +28,7 @@ int vasprintf(char **out, WATCHMAN_FMT_STRING(const char *fmt), va_list ap) {
 
     // Need a bigger boat
     alloc *= 2;
-    newbuf = realloc(buf == stack ? NULL : buf, alloc);
+    newbuf = (char*)realloc(buf == stack ? NULL : buf, alloc);
     if (!newbuf) {
       if (buf != stack) {
         free(buf);
