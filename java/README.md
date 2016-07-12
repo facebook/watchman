ab949480c1f4d52655bffdd05b0ce998de993d89
Cotizo Sima <cotizo@fb.com>
2016-07-12 16:55:34 +0100

    switching from jar dependencies to deps downloaded through a bash script

--- java/README.md
+++ java/README.md
@@ -6,6 +6,14 @@ This provides Java bindings to the Watchman service.
 Building
 ===
 
+First, you must make sure that you have all the required dependencies. You can
+either download them manually (please read the individual README files in each
+folder under third-party/), or you can run the auto-downloader:
+
+```
+bash deps.sh
+```
+
 Make sure that you have [buck](https://buckbuild.com/) installed. In this
 folder, run:
 
---
+++ java/deps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+curl http://central.maven.org/maven2/com/google/guava/guava/19.0/guava-19.0.jar -o third-party/guava/guava-19.0.jar
+
+curl http://central.maven.org/maven2/org/hamcrest/hamcrest-junit/2.0.0.0/hamcrest-junit-2.0.0.0.jar -o third-party/hamcrest/hamcrest-junit-2.0.0.0.jar
+curl http://central.maven.org/maven2/org/hamcrest/java-hamcrest/2.0.0.0/java-hamcrest-2.0.0.0.jar -o third-party/hamcrest/java-hamcrest-2.0.0.0.jar
+
+curl http://central.maven.org/maven2/org/immutables/generator/2.1.5/generator-2.1.5.jar -o third-party/immutables/generator-2.1.5.jar
+curl http://central.maven.org/maven2/org/immutables/value-processor/2.1.5/value-processor-2.1.5.jar -o third-party/immutables/value-processor-2.1.5.jar
+curl http://central.maven.org/maven2/org/immutables/value/2.1.5/value-2.1.5.jar -o third-party/immutables/value-2.1.5.jar
+
+curl http://central.maven.org/maven2/net/java/dev/jna/jna/4.2.0/jna-4.2.0.jar -o third-party/jna/jna-4.2.0.jar
+
+curl http://central.maven.org/maven2/com/google/code/findbugs/jsr305/3.0.1/jsr305-3.0.1.jar -o third-party/jsr-305/jsr305.jar
+
+curl http://central.maven.org/maven2/junit/junit/4.12/junit-4.12.jar -o third-party/junit/junit-4.12.jar
+
+curl http://central.maven.org/maven2/org/mockito/mockito-core/1.9.5/mockito-core-1.9.5.jar -o third-party/mockito/mockito-core-1.9.5.jar
+
+curl http://central.maven.org/maven2/com/zaxxer/nuprocess/1.1.0/nuprocess-1.1.0.jar -o third-party/nuprocess/nuprocess-1.1.0.jar
+
+curl http://central.maven.org/maven2/org/objenesis/objenesis/1.3/objenesis-1.3.jar -o third-party/objenesis/objenesis-1.3.jar
+
