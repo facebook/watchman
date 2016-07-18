53507eaae600f4d714aad6c40d6505482c6d2f99
Cotizo Sima <cotizo@fb.com>
2016-07-18 13:26:57 +0100

    fixing Travis runs when the flag for Java tests is not present

--- travis/run.sh
+++ travis/run.sh
@@ -59,7 +59,7 @@ find $INST_TEST
 
 case $(uname) in
   Darwin)
-    if [ "${BUILD_JAVA_CLIENT-0}" -eq 1 ]; then
+    if [[ "$BUILD_JAVA_CLIENT" != "" ]] && [ "${BUILD_JAVA_CLIENT-0}" -eq 1 ]; then
       pushd java
       buck fetch :watchman :watchman-tests || exit 1
       buck test :watchman || exit 1
