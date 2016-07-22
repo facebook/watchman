55e5bb037990e26e32e13581751b6f87b3cfe503
Dhruv Singhal <dhruv@singhal.me>
2016-07-21 17:43:28 -0700

    Changes json_string_nocheck to typed_string_to_json, annotating the strings with correct types in cmds/query.c

--- cmds/query.c
+++ cmds/query.c
@@ -64,7 +64,7 @@ static void cmd_query(struct watchman_client *client, json_t *args)
 
   response = make_response();
   if (clock_id_string(res.root_number, res.ticks, clockbuf, sizeof(clockbuf))) {
-    set_prop(response, "clock", json_string_nocheck(clockbuf));
+    set_unicode_prop(response, "clock", clockbuf);
   }
   set_prop(response, "is_fresh_instance",
            json_pack("b", res.is_fresh_instance));
