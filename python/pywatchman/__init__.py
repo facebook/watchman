823179f73878df647faa0e79d57e989e3c3da143
Dhruv Singhal <dhruvs@fb.com>
2016-08-18 14:35:13 -0700

    Reenabled fallback for BSER v2 codec.

--- python/pywatchman/__init__.py
+++ python/pywatchman/__init__.py
@@ -631,17 +631,15 @@ class Bser2WithFallbackCodec(BserCodec):
 
         capabilities = self.receive()
 
-        # Force the experimental v2 codec to be used. Reactivate fallback
-        # once this codec becomes the default.
-        #if 'error' in capabilities:
-        #  raise Exception('Unsupported BSER version')
+        if 'error' in capabilities:
+            raise Exception('Unsupported BSER version')
 
-        #if capabilities['capabilities']['bser-v2']:
-        self.bser_version = 2
-        self.bser_capabilities = 0
-        #else:
-        #    self.bser_version = 1
-        #    self.bser_capabilities = 0
+        if capabilities['capabilities']['bser-v2']:
+            self.bser_version = 2
+            self.bser_capabilities = 0
+        else:
+            self.bser_version = 1
+            self.bser_capabilities = 0
 
     def _loads(self, response):
         return bser.loads(response)
