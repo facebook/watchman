cc476197e09adb960eaf2d69e525c1de722c041b
Wez Furlong <wez@fb.com>
2016-08-18 08:11:24 -0700

    disable the dir move test on windows
    It's too flaky for reasons outside our control.

--- tests/integration/test_dir_move.py
+++ tests/integration/test_dir_move.py
@@ -14,8 +14,15 @@ import os.path
 import time
 import shutil
 import time
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 
+# testing this is flaky at best on windows due to latency
+# and exclusivity of file handles, so skip it.
+@unittest.skipIf(os.name == 'nt', 'win')
 class TestDirMove(WatchmanTestCase.WatchmanTestCase):
     def build_under(self, root, name, latency=0):
         os.mkdir(os.path.join(root, name))
