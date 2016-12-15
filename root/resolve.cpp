a2c5145baa5250777752720e027a4f777c8efe56
Yiqiu Liu <yiqiuliu0511@gmail.com>
2016-12-15 14:11:52 -0800

    add casing problem detection and test
    Summary:
    1. Add code into root.c to detect casing problem in case insensitive operation system.
    2. Create NodeJS test case.js to test this functionality.
    Closes https://github.com/facebook/watchman/pull/311
    
    Differential Revision: D4333005
    
    Reviewed By: sid0
    
    Pulled By: wez
    
    fbshipit-source-id: 5dede5fde16edbd75c4366f7e6b25d4742aa20ed

--- root/resolve.cpp
+++ root/resolve.cpp
@@ -128,6 +128,30 @@ std::shared_ptr<w_root_t> root_resolve(
   watch_path = w_realpath(filename);
   realpath_err = errno;
 
+  if (watch_path) {
+    struct stat st;
+    int res = w_lstat(filename, &st, true);
+
+    if (res != 0 && errno == ENOENT) {
+      ignore_result(asprintf(
+          errmsg,
+          "\"%s\" resolved to \"%s\" but we were unable to examine \"%s\" using strict "
+          "case sensitive rules.  Please check each component of the path and make "
+          "sure that that path exactly matches the correct case of the files on your "
+          "filesystem.",
+          filename,
+          watch_path,
+          filename));
+      w_log(W_LOG_ERR, "resolve_root: %s", *errmsg);
+      return nullptr;
+    } else if (res != 0) {
+      ignore_result(
+          asprintf(errmsg, "unable to lstat \"%s\" %d", filename, errno));
+      w_log(W_LOG_ERR, "resolve_root: %s", *errmsg);
+      return nullptr;
+    }
+  }
+
   if (!watch_path) {
     watch_path = (char*)filename;
   }
