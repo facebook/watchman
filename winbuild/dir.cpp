2944010a0c839bfe327a5ad770d5886a4f176479
Wez Furlong <wez@fb.com>
2016-12-19 14:08:30 -0800

    speed up directory crawling on windows
    Summary:
    this takes advantage of the pre-fetched file information that is
    available to us from the particular flavor of directory walking that we use on
    windows; we've already fetched all of the stat-like information that we
    request in our `lstat` implementation (see `watchman/winbuild/stat.cpp`),
    so we can fill out that data in our readdir API and set the has-stat flag
    to true.
    
    This causes the directory crawler to avoid making an additional `lstat` call.
    
    I haven't benchmarked this on a real system yet, but I suspect the gains are
    likely to be larger than you might think; my gut feeling is that by pre-fetching
    this information with the buffer size that we use, we are likely a good bit
    more expensive than the simplist `FindFilesXXX` API, only to thrash some caches
    with the follow up `lstat` calls--those need to open handles and query two separate
    sets of information.
    
    So I'm hoping that this has a pretty decent gain.
    
    Reviewed By: farnz
    
    Differential Revision: D4338468
    
    fbshipit-source-id: cffc0b7cebf97d4e07614d05a79ee4c6a187525a

--- winbuild/dir.cpp
+++ winbuild/dir.cpp
@@ -39,8 +39,12 @@ class WinDirHandle : public watchman_dir_handle {
           std::string("CreateFileW for opendir: ") + path);
     }
 
+    memset(&ent_, 0, sizeof(ent_));
     ent_.d_name = nameBuf_;
-    ent_.has_stat = false;
+    ent_.has_stat = true;
+    if (path[1] == ':') {
+      ent_.stat.dev = tolower(path[0]) - 'a';
+    }
   }
 
   const watchman_dir_ent* readDir() override {
@@ -75,6 +79,28 @@ class WinDirHandle : public watchman_dir_handle {
     }
 
     nameBuf_[len] = 0;
+
+    // Populate stat info to speed up the crawler() routine
+    FILETIME_LARGE_INTEGER_to_timespec(info_->CreationTime, &ent_.stat.ctime);
+    FILETIME_LARGE_INTEGER_to_timespec(info_->LastAccessTime, &ent_.stat.atime);
+    FILETIME_LARGE_INTEGER_to_timespec(info_->LastWriteTime, &ent_.stat.mtime);
+    ent_.stat.size = info_->EndOfFile.QuadPart;
+
+    if (info_->FileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) {
+      // This is a symlink, but msvcrt has no way to indicate that.
+      // We'll treat it as a regular file until we have a better
+      // representation :-/
+      ent_.stat.mode = _S_IFREG;
+    } else if (info_->FileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+      ent_.stat.mode = _S_IFDIR | S_IEXEC | S_IXGRP | S_IXOTH;
+    } else {
+      ent_.stat.mode = _S_IFREG;
+    }
+    if (info_->FileAttributes & FILE_ATTRIBUTE_READONLY) {
+      ent_.stat.mode |= 0444;
+    } else {
+      ent_.stat.mode |= 0666;
+    }
 
     // Advance the pointer to the next entry ready for the next read
     info_ = info_->NextEntryOffset == 0
