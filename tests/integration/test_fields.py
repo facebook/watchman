46c915fb19f07c5383d87d1d0e1a9e5b605bbbfc
Wez Furlong <wez@fb.com>
2018-03-01 22:03:57 -0800

    migrate fields.php to python
    Summary: as above
    
    Reviewed By: simpkins
    
    Differential Revision: D7128273
    
    fbshipit-source-id: 1a6364040dccec4964820bbb181eabd516e9006c

---
+++ tests/integration/test_fields.py
@@ -0,0 +1,81 @@
+# vim:ts=4:sw=4:et:
+# Copyright 2012-present Facebook, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# no unicode literals
+
+import WatchmanTestCase
+import os
+
+
+@WatchmanTestCase.expand_matrix
+class TestFields(WatchmanTestCase.WatchmanTestCase):
+
+    def test_fields(self):
+        root = self.mkdtemp()
+        self.watchmanCommand('watch', root)
+        self.touchRelative(root, 'a')
+        self.assertFileList(root, files=['a'])
+
+        result = self.watchmanCommand('query', root, {
+            'fields': [
+                'name',
+                'exists',
+                'new',
+                'size',
+                'mode',
+                'uid',
+                'gid',
+                'mtime',
+                'mtime_ms',
+                'mtime_us',
+                'mtime_ns',
+                'mtime_f',
+                'ctime',
+                'ctime_ms',
+                'ctime_us',
+                'ctime_ns',
+                'ctime_f',
+                'ino',
+                'dev',
+                'nlink',
+                'oclock',
+                'cclock',
+            ],
+            'since': 'n:foo'})
+        self.assertEqual(len(result['files']), 1)
+        file = result['files'][0]
+        self.assertEqual(file['name'], 'a')
+        self.assertTrue(file['exists'])
+        self.assertTrue(file['new'])
+
+        st = os.lstat(os.path.join(root, 'a'))
+
+        fields = ['size', 'mode', 'uid', 'gid']
+        if os.name != 'nt':
+            # These fields are meaningless in msvcrt
+            fields += ['dev', 'ino']
+            # Python seemingly has different logic for nlink than
+            # watchman and php on nt
+            fields += ['nlink']
+
+        for field in fields:
+            self.assertEqual(file[field], getattr(st, 'st_' + field),
+                msg=field)
+
+        for field in ['mtime', 'ctime']:
+            self.assertEqual(file[field], int(getattr(st, 'st_' + field)),
+                msg=field)
+            seconds = file[field]
+            ms = file[field + '_ms']
+            us = file[field + '_us']
+            ns = file[field + '_ns']
+            self.assertEqual(ms // 1000, seconds)
+            self.assertEqual(us // 1000, ms)
+            self.assertEqual(ns // 1000, us)
+
+        for field in ['cclock', 'oclock']:
+            self.assertRegexpMatches(file[field], '^c:\d+:\d+:\d+:\d+$')
