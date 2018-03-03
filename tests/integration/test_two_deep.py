109afc0a61a960b04d505119bed7952549f91519
Wez Furlong <wez@fb.com>
2018-03-02 20:29:13 -0800

    migrate twodeep.php to python
    Summary:
    test_fields.py has covered for the validity of fields
    in general, so this test can just focus on the mtime and depth
    relation that the original was testing.
    
    Reviewed By: simpkins
    
    Differential Revision: D7133601
    
    fbshipit-source-id: c962f7cf65765bb17941ed085494278a6f0770ce

---
+++ tests/integration/test_two_deep.py
@@ -0,0 +1,38 @@
+# vim:ts=4:sw=4:et:
+# Copyright 2012-present Facebook, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# no unicode literals
+
+import WatchmanTestCase
+import os
+import time
+
+
+@WatchmanTestCase.expand_matrix
+class TestTwoDeep(WatchmanTestCase.WatchmanTestCase):
+    def test_two_deep(self):
+        root = self.mkdtemp()
+        self.watchmanCommand('watch', root)
+
+        os.makedirs(os.path.join(root, 'foo', 'bar'))
+
+        # Guarantee that 111's mtime is greater than its parent dirs
+        time.sleep(1)
+
+        with open(os.path.join(root, 'foo', 'bar', '111'), 'w') as f:
+            f.write('111')
+
+        self.assertFileList(root, files=['foo', 'foo/bar', 'foo/bar/111'])
+
+        res_111 = self.watchmanCommand('find', root, 'foo/bar/111')['files'][0]
+        st_111 = os.lstat(os.path.join(root, 'foo', 'bar', '111'))
+
+        res_bar = self.watchmanCommand('find', root, 'foo/bar')['files'][0]
+        st_bar = os.lstat(os.path.join(root, 'foo', 'bar'))
+
+        self.assertEqual(res_111['mtime'], int(st_111.st_mtime))
+        self.assertEqual(res_bar['mtime'], int(st_bar.st_mtime))
