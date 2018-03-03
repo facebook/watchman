ea2c6ceba70965d755bfd912222ff61b14ae5358
Wez Furlong <wez@fb.com>
2018-03-02 20:29:10 -0800

    migrate type.php
    Summary: as above
    
    Reviewed By: simpkins
    
    Differential Revision: D7133610
    
    fbshipit-source-id: 9e66f45298dd05cb43898384185befee45e76afc

---
+++ tests/integration/test_type.py
@@ -0,0 +1,48 @@
+# vim:ts=4:sw=4:et:
+# Copyright 2012-present Facebook, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# no unicode literals
+
+import WatchmanTestCase
+import os
+import pywatchman
+
+
+@WatchmanTestCase.expand_matrix
+class TestTypeExpr(WatchmanTestCase.WatchmanTestCase):
+    def test_type_expr(self):
+        root = self.mkdtemp()
+
+        self.touchRelative(root, 'foo.c')
+        os.mkdir(os.path.join(root, 'subdir'))
+        self.touchRelative(root, 'subdir', 'bar.txt')
+
+        self.watchmanCommand('watch', root)
+
+        self.assertFileListsEqual(
+            self.watchmanCommand('query', root, {
+                'expression': ['type', 'f'],
+                'fields': ['name']})['files'],
+            ['foo.c', 'subdir/bar.txt'])
+
+        self.assertEqual(
+            self.watchmanCommand('query', root, {
+                'expression': ['type', 'd'],
+                'fields': ['name', 'type']})['files'],
+            [{'name': 'subdir', 'type': 'd'}])
+
+        with self.assertRaises(pywatchman.WatchmanError) as ctx:
+            self.watchmanCommand('query', root, {
+                'expression': ['type', 'x']})
+
+        self.assertIn("invalid type string 'x'", str(ctx.exception))
+
+        with self.assertRaises(pywatchman.WatchmanError) as ctx:
+            self.watchmanCommand('query', root, {
+                'expression': 'type'})
+
+        self.assertIn('must use ["type", "typestr"]', str(ctx.exception))
