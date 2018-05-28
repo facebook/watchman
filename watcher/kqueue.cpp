5213ea9b578e8bd746f6ea1e46e17169faf1ddf0
Matthew Glazar <strager.nds@gmail.com>
2018-05-28 12:35:13 -0700

    Fix symlink changes with kqueue watcher
    The test_symlink tests consistently fail on macOS with the kqueue watcher:
    
        $ ./runtests.py --watcher kqueue --retry-flaky 0 tests/integration/test_symlink.py
        Eden not available because: No module named configparser
        PASS test_symlink.TestSymlinkCliJson.test_watchSymlinkTarget (0.239s)
        PASS test_symlink.TestSymlinkLocalBser2.test_watchRelativeSymlinkTargetOneHop (0.107s)
        PASS test_symlink.TestSymlinkCliJson.test_delSymlink (0.044s)
        PASS test_symlink.TestSymlinkLocalBser2.test_watchSymlinkToDirContainsSymlink (0.175s)
        FAIL test_symlink.TestSymlinkLocalBser.test_symlink (0.005s)
        Traceback (most recent call last):
          File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/unittest/case.py", line 331, in run
    	testMethod()
          File "/Users/mg/Projects/watchman/tests/integration/test_symlink.py", line 65, in test_symlink
    	self.assertEqual("333", os.path.basename(res["files"][0]["symlink_target"]))
          File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/unittest/case.py", line 515, in assertEqual
    	assertion_func(first, second, msg=msg)
          File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/unittest/case.py", line 508, in _baseAssertEqual
    	raise self.failureException(msg)
        AssertionError: '333' != '222'
        [snip]
        FAIL test_symlink.TestSymlinkCliJson.test_symlinkToFileDir (0.040s)
        Traceback (most recent call last):
          File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/unittest/case.py", line 331, in run
    	testMethod()
          File "/Users/mg/Projects/watchman/tests/integration/test_symlink.py", line 142, in test_symlinkToFileDir
    	or res["files"][0]["symlink_target"] is None
          File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/unittest/case.py", line 424, in assertTrue
    	raise self.failureException(msg)
        AssertionError: False is not true
        [snip]
        Ran 40, failed 7, skipped 4, concurrency 1
    
    The test failures are caused by tracking changes in the symlink's target
    instead of tracking changes in the symlink itself. Use open()'s
    O_SYMLINK flag to track changes to the symlink itself, fixing the tests:
    
        $ ./runtests.py --watcher kqueue --retry-flaky 0 tests/integration/test_symlink.py
        Eden not available because: No module named configparser
        PASS test_symlink.TestSymlinkLocalJson.test_delSymlink (0.006s)
        PASS test_symlink.TestSymlinkLocalJson.test_watchRelativeSymlinkTarget (0.106s)
        PASS test_symlink.TestSymlinkLocalJson.test_watchSymlinkTargetLinkToLink (0.114s)
        PASS test_symlink.TestSymlinkCliJson.test_watchRelativeSymlinkTargetOneHop (0.142s)
        SKIP test_symlink.TestSymlinkCliJson.test_invalidSymlink (0.000s) known fsevents bug
        PASS test_symlink.TestSymlinkCliJson.test_delSymlink (0.043s)
        PASS test_symlink.TestSymlinkLocalBser.test_watchSymlinkTarget (0.164s)
        PASS test_symlink.TestSymlinkLocalBser2.test_delSymlink (0.004s)
        SKIP test_symlink.TestSymlinkLocalBser.test_invalidSymlink (0.000s) known fsevents bug
        [snip]
        Ran 40, failed 0, skipped 4, concurrency 1

--- watcher/kqueue.cpp
+++ watcher/kqueue.cpp
@@ -82,7 +82,11 @@ bool KQueueWatcher::startWatchFile(struct watchman_file* file) {
 
   w_log(W_LOG_DBG, "watch_file(%s)\n", full_name.c_str());
 
-  FileDescriptor fdHolder(open(full_name.c_str(), O_EVTONLY | O_CLOEXEC));
+  int openFlags = O_EVTONLY | O_CLOEXEC;
+#if HAVE_DECL_O_SYMLINK
+  openFlags |= O_SYMLINK;
+#endif
+  FileDescriptor fdHolder(open(full_name.c_str(), openFlags));
 
   auto rawFd = fdHolder.fd();
 
