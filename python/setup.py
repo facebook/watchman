9226bae49da4e33d3153fa05e22968ebdbab1c55
Wez Furlong <wez@fb.com>
2019-05-20 19:23:34 -0700

    watchman: simplify the cmake invocation of setup.py
    Summary:
    This diff teaches setup.py about out of source builds
    by passing the source and binary dirs into it from cmake.
    
    This results in a simpler way to perform the setup.py install
    step without bleeding knowledge of python internals out into
    the cmake file
    
    Reviewed By: simpkins
    
    Differential Revision: D15300087
    
    fbshipit-source-id: 35e61978ae66a40a2dd368dea8127315af6a551f

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,31 @@
 import os
 
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+# To support out-of-source builds, we distinguish between the build
+# dir and the source dir.  The watchman cmake file arranges to
+# export the source and binary dirs when it invokes us.  If they're
+# not set then we assume that the build dir == source dir.
+src_dir = os.path.dirname(os.path.realpath(__file__))
+build_dir = os.environ.get("CMAKE_CURRENT_BINARY_DIR", src_dir)
+src_dir = os.environ.get("CMAKE_CURRENT_SOURCE_DIR", src_dir)
+
+# Execute from the build dir
+os.chdir(build_dir)
+
+# The python source dir.
+# On Windows, this has to be relative to the cwd otherwise something
+# in the setuptools machinery does the wrong thing and produces a
+# path like `Z:blah` which on windows resolves ambiguously depending
+# on the cwd.
+py_dir = os.path.join(src_dir, "python")
+if os.name == "nt":
+    py_dir = os.path.relpath(py_dir)
+
+
+def srcs(names):
+    """ transform a list of sources to be relative to py_dir """
+    return ["%s/%s" % (py_dir, n) for n in names]
+
 
 try:
     from setuptools import setup, Extension
@@ -14,6 +38,7 @@ except:
 setup(
     name="pywatchman",
     version="1.4.1",
+    package_dir={"": py_dir},
     description="Watchman client for python",
     author="Wez Furlong, Rain",
     author_email="wez@fb.com",
@@ -24,7 +49,7 @@ setup(
     keywords=("watchman inotify fsevents kevent kqueue portfs filesystem watcher"),
     license="BSD",
     packages=["pywatchman"],
-    ext_modules=[Extension("pywatchman.bser", sources=["pywatchman/bser.c"])],
+    ext_modules=[Extension("pywatchman.bser", sources=srcs(["pywatchman/bser.c"]))],
     platforms="Platform Independent",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -40,10 +65,12 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     zip_safe=True,
-    scripts=[
-        "bin/watchman-make",
-        "bin/watchman-wait",
-        "bin/watchman-replicate-subscription",
-    ],
+    scripts=srcs(
+        [
+            "bin/watchman-make",
+            "bin/watchman-wait",
+            "bin/watchman-replicate-subscription",
+        ]
+    ),
     test_suite="tests",
 )
