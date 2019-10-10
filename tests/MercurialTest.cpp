2c8f141b3c386c1d24f0b9c2534fbc7762377d31
Wez Furlong <wez@fb.com>
2019-10-09 20:59:34 -0700

    watchman: migrate mercurial test to gtest
    Summary: as above
    
    Reviewed By: pkaush
    
    Differential Revision: D17775681
    
    fbshipit-source-id: 4af446882df9136fb6308b37399eb671e4ef5c0d

--- tests/MercurialTest.cpp
+++ tests/MercurialTest.cpp
@@ -1,22 +1,16 @@
 /* Copyright 2016-present Facebook, Inc.
  * Licensed under the Apache License, Version 2.0. */
 
-#include "scm/Mercurial.h"
-#include "thirdparty/tap.h"
 #include "watchman.h"
+#include "scm/Mercurial.h"
+#include <folly/logging/xlog.h>
+#include <folly/portability/GTest.h>
 
 using namespace std::chrono;
 
-void test_convert_date() {
+TEST(Mercurial, convertCommitDate) {
   auto date = watchman::Mercurial::convertCommitDate("1529420960.025200");
   auto result = duration_cast<seconds>(date.time_since_epoch()).count();
   auto expected = 1529420960;
-  ok(result == expected, "Expected %d but observed %d", expected, result);
-}
-
-int main(int, char**) {
-  plan_tests(1);
-  test_convert_date();
-
-  return exit_status();
+  EXPECT_EQ(result, expected);
 }
