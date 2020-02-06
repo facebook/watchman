e090d707bde02b1d2aa7f27524a1360bd60f439c
Adam Simpkins <simpkins@fb.com>
2020-02-06 11:16:16 -0800

    move signal-handling logic to a new signal.cpp file
    Summary:
    Move the signal-handling code from `log.cpp` to a new `signal.cpp` file.
    This eliminates the dependency from `log.cpp` on `listener.cpp`, which allows
    tests to use `log.cpp` without linking in the full `watchmanlib` library or
    having to provide stub implementations of parts of `listener.cpp`.
    
    Reviewed By: chadaustin
    
    Differential Revision: D19738512
    
    fbshipit-source-id: b8704d4cf29eab3a0f8034392423167b493fe59d

--- tests/log_stub.cpp
+++
@@ -1,7 +0,0 @@
-/* Copyright 2016-present Facebook, Inc.
- * Licensed under the Apache License, Version 2.0. */
-
-// These are logging stubs to facilitate testing code that pulls in w_log
-// either directly or indirectly.
-
-void w_request_shutdown(void) {}
