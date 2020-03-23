63f372f9327c5e01002a3caa2c5a70db6fd519dc
Lee Howes <lwh@fb.com>
2020-03-23 12:23:59 -0700

    collectX to collectXSemiFuture or collectXUnsafe
    Summary:
    Migration from Future-returning executor-erasing collectX forms to
    SemiFuture-returning forms, that are less risky in particular with coroutines.
    
    Earlier changes added collectXSemiFuture and collectXUnsafe as a migration
    path. This diff is part of a migration off of the basic collectX forms where
    there might be some conflict with the change of return type in Phase 4.
    
    Reviewed By: wez
    
    Differential Revision: D20589484
    
    fbshipit-source-id: 7e4016d44a4f64af598aa65fa710b820d0fc8dfe

--- tests/CacheTest.cpp
+++ tests/CacheTest.cpp
@@ -110,8 +110,8 @@ TEST(CacheTest, future) {
   EXPECT_EQ(cache.size(), 5)
       << "cache should be full, but has " << cache.size();
 
-  folly::collectAll(futures.begin(), futures.end())
-      .thenTry(
+  folly::collectAllSemiFuture(futures.begin(), futures.end())
+      .defer(
           [](folly::Try<std::vector<folly::Try<std::shared_ptr<const Node>>>>&&
                  result) {
             for (auto& r : result.value()) {
