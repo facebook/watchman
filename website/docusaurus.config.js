19a046ea05d67d5747efbd6a912640abddfa9492
Michael Bolin <mbolin@fb.com>
2020-03-24 20:18:20 -0700

    Upgrade Prettier from 1.17 to 2.0.2.
    Summary:
    This gets us on the latest Prettier 2.x:
    https://prettier.io/blog/2020/03/21/2.0.0.html
    
    Notably, this adds support for TypeScript 3.8,
    which introduces new syntax, such as `import type`.
    
    Reviewed By: zertosh
    
    Differential Revision: D20636268
    
    fbshipit-source-id: fca5833d003804333a05ba16325bbbe0e06d6c8a

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -53,8 +53,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/facebook/watchman/edit/master/website/',
+          editUrl: 'https://github.com/facebook/watchman/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
