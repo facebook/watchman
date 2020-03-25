66f04d9ea2622e18f576f6ca73f0aa8e0e289b98
Yangshun Tay <tay.yang.shun@gmail.com>
2020-03-25 09:07:19 -0700

    Improve Docusaurus website set-up (#791)
    Summary:
    - Home page
      - Make landing page nicer
      - Remove unused CSS
      - Fix duplicate title appearing in meta title
    - Docs
      - Fix broken image path in docs
    - Fix docusaurus config
    Pull Request resolved: https://github.com/facebook/watchman/pull/791
    
    Test Plan:
    Nicer home page
    
    <img width="1552" alt="Screen Shot 2020-03-25 at 11 18 29 PM" src="https://user-images.githubusercontent.com/1315101/77552803-f6328800-6eee-11ea-98ef-5f4ceef46049.png">
    
    Fixed broken images in docs
    
    <img width="1552" alt="Screen Shot 2020-03-25 at 11 48 43 PM" src="https://user-images.githubusercontent.com/1315101/77556505-92f72480-6ef3-11ea-8513-46d8d5876928.png">
    
    cc fanny for your information. Since I didn't get to review it before it was merged, I'll address the issues myself.
    
    wez
    
    Reviewed By: fanny
    
    Differential Revision: D20646765
    
    Pulled By: wez
    
    fbshipit-source-id: e0439530985f59081c29b4aef6e46f89cdbb548f

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,7 +10,7 @@
 module.exports = {
   title: 'Watchman',
   tagline: 'A file watching service',
-  url: 'https://facebook.github.io/watchman/',
+  url: 'https://facebook.github.io',
   baseUrl: '/watchman/',
   favicon: 'img/favicon.png',
   organizationName: 'facebook',
@@ -53,8 +53,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/facebook/watchman/edit/master/website/',
+          editUrl: 'https://github.com/facebook/watchman/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
