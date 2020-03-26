0d8b8faace2b767f4b4f0b3b267354d9abed97f8
Wez Furlong <wez@fb.com>
2020-03-26 05:46:49 -0700

    watchman: remove dead code
    Summary:
    These have all been superseded by the newer shared
    getdeps.py found under build/fbcode_builder.
    
    We don't use travis any more.
    
    Reviewed By: chadaustin
    
    Differential Revision: D20649679
    
    fbshipit-source-id: 42b78fc5b8f82da7035573071e610f938b966ec6

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,6 @@
 #!/bin/bash -e
 # vim:ts=2:sw=2:et:
 set -x
-
-case "$OSTYPE" in
-  darwin*)
-    # Ensure that we pick up the homebrew openssl installation
-    if command -v brew >/dev/null ; then
-      export PKG_CONFIG_PATH="$(brew --prefix)/opt/openssl/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
-    fi
-    ;;
-esac
-
-if test ! -d external ; then
-  ./getdeps.py --install-deps
-fi
-
-cmake .
+python3 build/fbcode_builder/getdeps.py build --src-dir=. watchman
+python3 build/fbcode_builder/getdeps.py fixup-dyn-deps --src-dir=. watchman built --final-install-prefix /usr/local
+find built -ls
