2b8ad08415ce036db85c21b59f3029246d5b6c72
Chad Austin <chadaustin@fb.com>
2020-10-07 14:13:49 -0700

    light journal refactoring and comment improvements
    Summary:
    Before I make further changes to the Journal, improve the comments and
    refactor a few small things.
    
    Reviewed By: kmancini
    
    Differential Revision: D24089530
    
    fbshipit-source-id: de9da2c1e6b1c87b6587781cfa55ae7cc4085eeb

--- eden/fs/service/streamingeden.thrift
+++ eden/fs/service/streamingeden.thrift
@@ -14,20 +14,24 @@ namespace py3 eden.fs.service
  * language/runtimes. */
 
 service StreamingEdenService extends eden.EdenService {
-  /** Request notification about changes to the journal for
+  /**
+   * Request notification about changes to the journal for
    * the specified mountPoint.
-   * The JournalPosition at the time of the subscribe call
-   * will be pushed to the client, and then each change will
-   * be pushed to the client in near-real-time.
-   * The client may then use methods like getFilesChangedSince()
-   * to determine the precise nature of the changes.
+   *
+   * Do not use the JournalPosition values in the stream. Instead,
+   * call getFilesChangedSince or getCurrentJournalPosition which
+   * will return up-to-date information and unblock future notifications
+   * on this subscription. If the subscriber never calls getFilesChangedSince
+   * or getCurrentJournalPosition in response to a notification on this
+   * stream, future notifications may not arrive.
    *
    * This is an implementation of the subscribe API using the
    * new rsocket based streaming thrift protocol.
    * The name is temporary: we want to make some API changes
    * but want to start pushing out an implementation now because
    * we've seen inflated memory usage for the older `subscribe`
-   * method above. */
+   * method above.
+   */
   stream<eden.JournalPosition> subscribeStreamTemporary(
     1: string mountPoint)
 }
