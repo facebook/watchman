bca70f0bfaa2da0de4603ddecf042763f794ee79
Chad Austin <chadaustin@fb.com>
2021-08-31 13:03:15 -0700

    relicense as MIT
    Summary:
    To bring the pywatchman and Ruby bindings in sync with the rest of
    Watchman, relicense from Apache 2 to MIT.
    
    Reviewed By: syotfs
    
    Differential Revision: D30654289
    
    fbshipit-source-id: 4d6aeecd70d44a93ca9b770b347d9e26017e9ace

--- watchman/python/tests/tests.py
+++ watchman/python/tests/tests.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # vim:ts=4:sw=4:et:
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from __future__ import absolute_import, division, print_function
 
 import binascii
