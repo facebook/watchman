cffe7baa0213bd020bc0f04723fecec9be9f949a
Chad Austin <chadaustin@fb.com>
2021-10-13 17:18:20 -0700

    write a test that instantiates an InMemoryView
    Summary:
    Being able to create an InMemoryView in a unit test is a first step
    towards testing the iothread's event loop.
    
    Reviewed By: zhengchaol
    
    Differential Revision: D31348880
    
    fbshipit-source-id: e1b6e62f09c6d90ae862623a8ee08ef7cdf2b1a7

---
+++ watchman/tests/InMemoryViewTest.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "watchman/InMemoryView.h"
+#include <folly/portability/GTest.h>
+#include "watchman/FSDetect.h"
+#include "watchman/watcher/Watcher.h"
+
+namespace {
+
+using namespace ::testing;
+using namespace watchman;
+
+class FakeWatcher : public Watcher {
+ public:
+  FakeWatcher() : Watcher{"fake", 0} {}
+
+  std::unique_ptr<watchman_dir_handle> startWatchDir(
+      const std::shared_ptr<Root>& root,
+      struct watchman_dir* dir,
+      const char* path) override {
+    (void)root;
+    (void)dir;
+    (void)path;
+    throw "boom";
+  }
+
+  bool waitNotify(int timeoutms) override {
+    (void)timeoutms;
+    throw "boom";
+  }
+
+  ConsumeNotifyRet consumeNotify(
+      const std::shared_ptr<Root>& root,
+      PendingChanges& coll) override {
+    (void)root;
+    (void)coll;
+    throw "boom";
+  }
+};
+
+TEST(InMemoryViewTest, can_construct) {
+  Configuration config;
+  auto watcher = std::make_shared<FakeWatcher>();
+
+  InMemoryView view{"/fake/root", config, watcher};
+}
+
+} // namespace
