cba4f83fb7dc2685f1f720b3198125e076365ccb
Chad Austin <chadaustin@fb.com>
2021-10-13 20:34:07 -0700

    correctness fixes to GetCurrentDirectoryW call
    Summary:
    This is highly unlikely, but it looks like Watchman could segfault
    with a current working directory greater than 4096 characters, and
    remove an allocation while I'm at it.
    
    Reviewed By: fanzeyi
    
    Differential Revision: D31488916
    
    fbshipit-source-id: b9a537858b8f6375b60f3e1c43a1b1cdbe33403a

--- watchman/fs/FileDescriptor.cpp
+++ watchman/fs/FileDescriptor.cpp
@@ -716,6 +716,24 @@ w_string FileDescriptor::readSymbolicLink() const {
 #endif
 }
 
+#ifdef _WIN32
+namespace {
+w_string getCurrentDirectory() {
+  WCHAR wchar[WATCHMAN_NAME_MAX];
+  auto len = GetCurrentDirectoryW(std::size(wchar), wchar);
+  auto err = GetLastError();
+  // Technically, len > std::size(wchar) is sufficient, because the w_string
+  // constructor below will add a trailing zero.
+  if (len == 0 || len >= std::size(wchar)) {
+    throw std::system_error(
+        err, std::system_category(), "GetCurrentDirectoryW");
+  }
+  // Assumption: that the OS maintains the CWD in canonical form
+  return w_string(wchar, len);
+}
+} // namespace
+#endif
+
 w_string realPath(const char* path) {
   auto options = OpenFileHandleOptions::queryFileInfo();
   // Follow symlinks, because that's really the point of this function
@@ -723,23 +741,13 @@ w_string realPath(const char* path) {
   options.strictNameChecks = 0;
 
 #ifdef _WIN32
-  // Special cases for cwd
-  w_string_piece pathPiece(path);
+  // Special cases for cwd.
   // On Windows, "" is used to refer to the CWD.
   // We also allow using "." for parity with unix, even though that
   // doesn't generally work for that purpose on windows.
   // This allows `watchman watch-project .` to succeeed on windows.
-  if (pathPiece.size() == 0 || pathPiece == ".") {
-    std::wstring wchar;
-    wchar.resize(WATCHMAN_NAME_MAX);
-    auto len = GetCurrentDirectoryW(wchar.size(), &wchar[0]);
-    auto err = GetLastError();
-    if (len == 0) {
-      throw std::system_error(
-          err, std::system_category(), "GetCurrentDirectoryW");
-    }
-    // Assumption: that the OS maintains the CWD in canonical form
-    return w_string(wchar.data(), len);
+  if (path[0] == 0 || (path[0] == '.' && path[1] == 0)) {
+    return getCurrentDirectory();
   }
 #endif
 
