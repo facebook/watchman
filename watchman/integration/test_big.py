e1f74bef3b13dc988a387ad8949c3435b3ae945a
Chad Austin <chadaustin@fb.com>
2022-04-27 15:59:25 -0700

    make test_bigQuery much faster and more parallel by breaking it into separate tests
    Summary:
    test_bigQuery spawns the watchman CLI in a loop, which is painfully
    slow in dev builds. Split this test into multiple tests, one per four
    batch sizes.
    
    Reviewed By: kmancini
    
    Differential Revision: D35835067
    
    fbshipit-source-id: fc6d67f48c5ba1875ad6e704c3587b3025787c43

--- watchman/integration/test_big.py
+++ watchman/integration/test_big.py
@@ -6,30 +6,48 @@
 
 
 import os
+from typing import List
 
 import pywatchman
 from watchman.integration.lib import WatchmanTestCase
 
 
+def populate_tests(original_test_class):
+    # Rather than one long sequential test that tests every buffer size, register
+    # an individual test per size. This allows more tests to run in parallel.
+
+    # Create a huge query.  We're shooting for more than 2MB; the server buffer
+    # size is 1MB and we want to make sure we need more than 2 chunks, and we
+    # want to tickle some buffer boundary conditions
+
+    base = 2 * 1024 * 1024
+    sizes = list(range(base - 256, base + 256, 63))
+    while sizes:
+        batch = sizes[:4]
+        sizes = sizes[4:]
+        suffix = "_".join(map(str, batch))
+        setattr(
+            original_test_class,
+            f"test_bigQuery_{suffix}",
+            lambda self, batch=batch: self.do_test_bigQuery(sizes=batch),
+        )
+
+    return original_test_class
+
+
 @WatchmanTestCase.expand_matrix
+@populate_tests
 class TestBig(WatchmanTestCase.WatchmanTestCase):
     def checkOSApplicability(self) -> None:
         if os.name == "nt":
             self.skipTest("Windows has problems with this test")
 
-    def test_bigQuery(self) -> None:
+    def do_test_bigQuery(self, sizes: List[int]) -> None:
         root = self.mkdtemp()
 
         self.watchmanCommand("watch", root)
 
-        # Create a huge query.  We're shooting for more than 2MB;
-        # the server buffer size is 1MB and we want to make sure
-        # we need more than 2 chunks, and we want to tickle some
-        # buffer boundary conditions
-
-        base = 2 * 1024 * 1024
-
-        for size in range(base - 256, base + 2048, 63):
+        for size in sizes:
             try:
                 res = self.watchmanCommand(
                     "query", root, {"expression": ["name", "a" * size]}
