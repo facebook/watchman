daabc0c72e72c07c0ddef4f362a67209e60d9646
Jun Wu <quark@fb.com>
2022-06-27 08:48:10 -0700

    initial parallel directory walker implementation
    Summary:
    Similar to Rust's `jwalk` crate, walk directory in parallel and collect all the
    filenames and stats. On Mac Pro this can saturate IO and might provide 8-10x
    speedup.
    
    This is the initial implementation that does the job. To integrate with
    watchman, the missing pieces are:
    - Provide a way to have side effects (watcher->startWatchDir, ex.
      inotify_add_watch) before readDir for correctness. Or, make watchman
      skip parallel walking for directories that need extra side effects.
    - Provide a way to filter paths. Required by IgnoreSet.
    
    This diff also includes an executable for demostration. Let me know if I should
    move it to a separate diff.
    
    Reviewed By: chadaustin
    
    Differential Revision: D36937072
    
    fbshipit-source-id: 62e4f82316a406cc541494d18149e0827a57bb39

---
+++ watchman/fs/ParallelWalk.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <folly/ExceptionWrapper.h>
+#include <folly/FBString.h>
+#include <optional>
+#include <vector>
+#include "watchman/fs/FileInformation.h"
+#include "watchman/fs/FileSystem.h"
+
+/** Parallel filesystem walker. Collect path names and stats recursively. */
+
+namespace watchman {
+
+// Consider eden/fs/utils/PathFuncs.h, if builds.
+using PathComponent = folly::fbstring;
+using RelativePath = folly::fbstring;
+using AbsolutePath = folly::fbstring;
+
+/** Similar to `DirEntry` but always contains stat and owns the strings. */
+struct DirEntryOwned {
+  PathComponent name;
+  FileInformation stat;
+};
+
+/** ReadDir result: names and stats of direct children of a directory. */
+struct ReadDirResult {
+  // Directory path relative to the root.
+  RelativePath dirPath;
+
+  // Entries and their stats.
+  std::vector<DirEntryOwned> entries;
+};
+
+/** Error message with a path associated */
+struct IoErrorWithPath {
+  // Full path causing the error. Could be a file or a directory.
+  AbsolutePath fullPath;
+
+  // Actual error.
+  folly::exception_wrapper error;
+};
+
+struct ParallelWalkerContext;
+
+class ParallelWalker final {
+ public:
+  /**
+   * Start reading rootPath + startPath recursively. Does not block.
+   *
+   * Use nextResult() to obtain ReadDirResults.
+   * Use nextError() to obtain IoErrorWithPaths.
+   */
+  explicit ParallelWalker(
+      FileSystem& fileSystem,
+      AbsolutePath rootPath,
+      RelativePath startPath);
+
+  /**
+   * Obtain the next ReadDirResult. Might block.
+   *
+   * Parent directory is guarnateed to be provided before child directories.
+   * After completion, always return nullopt without blocking.
+   */
+  std::optional<ReadDirResult> nextResult();
+
+  /**
+   * Obtain an occured error. Might block.
+   *
+   * After completion, always return nullopt without blocking.
+   */
+  std::optional<IoErrorWithPath> nextError();
+
+  /**
+   * Discard the walker. Does not block.
+   *
+   * Existing tasks will still run to completion but new tasks will exit
+   * immediately without spawning tasks for subdirectories.
+   * context_ will be dropped after completeing all tasks.
+   */
+  ~ParallelWalker();
+
+  ParallelWalker() = delete;
+  ParallelWalker(ParallelWalker&&) = delete;
+  ParallelWalker(const ParallelWalker&) = delete;
+  ParallelWalker& operator=(ParallelWalker&&) = delete;
+  ParallelWalker& operator=(const ParallelWalker&) = delete;
+
+ private:
+  std::shared_ptr<ParallelWalkerContext> context_;
+};
+
+} // namespace watchman
