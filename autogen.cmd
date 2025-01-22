3d0fead630cf334d925e85f4231d505e9346eb6a
cttestid41 <cttestid41@gmail.com>
2025-01-22 18:20:06 +0530

    Update autogen.cmd

--- autogen.cmd
+++ autogen.cmd
@@ -2,4 +2,4 @@
 
 python3 build/fbcode_builder/getdeps.py build --src-dir=. watchman "--project-install-prefix=watchman:%userprofile%" --scratch-path "C:\open\scratch"
 
-python3 build/fbcode_builder/getdeps.py fixup-dyn-deps --src-dir=. watchman  built "--project-install-prefix=watchman:%userprofile%" --final-install-prefix "%userprofile%" --scratch-path "C:\open\scratch"
+python3 build/fbcode_builder/getdeps.py fixup-dyn-deps --src-dir=. watchman  built "--project-install-prefix=watchman:%userprofile%" --final-install-prefix "%userprofile%" --scratch-path "C:\open\scratch"fdfgfdbgfgb
